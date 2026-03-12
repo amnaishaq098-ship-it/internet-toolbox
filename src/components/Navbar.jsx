940b399d7ab11a7a936d8fbc68f988ab4ecd996e
amnaishaq098-ship-it <amnaishaq098@gmail.com>
2026-03-12 20:04:36 +0500

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+export default function Navbar(){
+
+return(
+
+<div className="navbar">
+
+<div className="logo">
+Internet Toolbox
+</div>
+
+<div className="navlinks">
+
+<a href="/">Home</a>
+<a href="#">Tools</a>
+<a href="#">Categories</a>
+<a href="#">API</a>
+
+</div>
+
+</div>
+
+)
+
+}
