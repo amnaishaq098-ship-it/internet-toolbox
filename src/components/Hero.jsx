51fcdfa8d51f3e5eb252089e8ccf07e7d33f6ff2
amnaishaq098-ship-it <amnaishaq098@gmail.com>
2026-03-12 20:05:01 +0500

    Create Hero.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+export default function Hero(){
+
+return(
+
+<div className="hero">
+
+<h1>
+All Your Internet Tools
+</h1>
+
+<p>
+Fast, powerful online tools for developers.
+</p>
+
+</div>
+
+)
+
+}
