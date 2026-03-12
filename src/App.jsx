597166cb62797c25e063436c16628adca4f7a441
amnaishaq098-ship-it <amnaishaq098@gmail.com>
2026-03-12 20:00:31 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,18 @@
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
 
+export default function App(){
+
+  return(
+
+    <div>
+
+      <Navbar />
+
+      <Home />
+
+    </div>
+
+  )
+
+}
